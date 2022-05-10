8fcf1ab041634452b74d2f10acadd952c6d5f88a
B0229123 <b0229123@APB-LTB0229123-MAC.local>
2022-05-10 10:30:13 +0530

    Added Flexbox

---
+++ flex-grid/script.js
@@ -0,0 +1,18 @@
+function chooseFrame(value)
+{
+    console.log('choose frame called',value);
+    if(value=='flex')
+        {
+            document.getElementById('grid').style.display='none';
+            document.getElementById('flex').style.display='block';
+        }
+    else if(value=='grid')
+        {
+            document.getElementById('flex').style.display='none';
+            document.getElementById('grid').style.display='block';
+        }
+    else if(value=='flex-demo')
+        location.href=location.origin+'/flex-grid/flex.html'
+    else
+        location.href=location.origin+'/flex-grid/grid.html' 
+}
