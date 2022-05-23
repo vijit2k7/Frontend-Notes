bcce9fe5cff7ac0564a5a1742991ff8d408238e1
B0229123 <b0229123@APB-LTB0229123-MAC.local>
2022-05-23 20:55:03 +0530

    Pushing callback

---
+++ javascript-basics/basics/callback.js
@@ -0,0 +1,46 @@
+// const message = function(a) {  
+
+//     console.log("This message is shown after 3 seconds",a);
+
+// }
+
+// setTimeout(()=>{console.log('hello')},2000);
+
+
+document.querySelector("#callback-btn")
+   .addEventListener("click", function() {    
+      console.log("User has clicked on the button!");
+});
+
+// var arr=[1,2,3]
+
+// var [m,n]=arr;
+
+// console.log(m,n,k);
+
+// var obj1={
+//     'Vijit':28,
+//     'Naman':30
+// }
+
+// var {Vijit,age}=obj1;
+// var a=obj1['Vijit']
+// var b=obj1['Naman']
+// console.log(Vijit);
+
+// var arr=[1,2,3,4,5]
+
+// var [a,num]=[1,...arr]
+
+
+
+
+
+
+
+
+
+
+
+
+
